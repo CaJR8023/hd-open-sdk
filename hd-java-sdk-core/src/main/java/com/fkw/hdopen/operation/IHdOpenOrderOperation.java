ba3d9024da59928fac5df6fffa29a00876fe95cf
林生 <389534510@qq.com>
2022-01-25 15:13:24 +0800

    【接口服务】标准接口和平台类接口服务新增支付信息：补充注释

--- hd-java-sdk-core/src/main/java/com/fkw/hdopen/operation/IHdOpenOrderOperation.java
+++ hd-java-sdk-core/src/main/java/com/fkw/hdopen/operation/IHdOpenOrderOperation.java
@@ -9,5 +9,15 @@ import com.fkw.hdopen.model.PageResult;
  * @Update HLS 2021/12/24 15:48
  */
 public interface IHdOpenOrderOperation {
+
+    /**
+     * 获取订单列表
+     * @param aid 凡科id
+     * @param activityId 活动id
+     * @param pageNo 页码
+     * @param pageSize 条目数
+     * @Author HLS 2021/12/24 15:48
+     * @Update HLS 2021/12/24 15:48
+     */
     PageResult<HdOpenOrderVO> getHdOrderInfoList(int aid, int activityId, Integer pageNo, Integer pageSize);
 }
